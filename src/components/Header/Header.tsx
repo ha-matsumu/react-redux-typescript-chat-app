77782ad5668cf58297ee0df3cc51c6f11b449d39
ha.ma <ha.matsumu@gmail.com>
2019-10-06 12:56:46 +0900

    css修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ const Navbar: React.SFC<Props> = ({ name, logout }) => {
       <div className="wrapper">
         <h2 className="logo">CHAT APP</h2>
         <div className="spacer" />
-        <h4 className="user-name">{"ようこそ, " + name + " さん"}</h4>
+        <h4 className="user-name">{name}</h4>
         {name !== "ゲスト" ? (
           <button className="logout-button" onClick={() => logout()}>
             ログアウト
